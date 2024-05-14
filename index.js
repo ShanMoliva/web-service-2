06df1f0cf744852d80289119d2aa19ed3379ed29
CCSLAB3-PC5\CSSLAB3 <obliteratedimagination@gmail.com>
2024-05-14 13:49:52 +0800

    no message

--- index.js
+++ index.js
@@ -4,38 +4,33 @@ const app = express();
 const port = 3000;
 
 const data = {
-  education: {
-    degree: 'Bachelor of Science in Information Technology (BSIT)',
-    university: 'University of Southern Philippines Foundation (USPF)'
-  },
-  skills: [
-    'Proficiency in front-end web technologies: HTML, CSS, JavaScript',
-    'Experience with responsive web design and cross-browser compatibility',
-    'Basic understanding of version control systems, such as Git',
-    'Strong teamwork and communication skills through collaborative project work'
+  education: [
+  { degree: 'Bachelor of Science in Information Technology (BSIT)', university: 'University of Southern Philippines Foundation (USPF)' }
   ],
-  experience: {
-    job1: 'Front End Developer / something.com',
-    description1: 'Highly skilled',
-    job2: 'Mobile Developer / something.com',
-    description2: 'Highly skilled',
-    job3: 'Web Developer / something.com',
-    description3: 'Highly skilled'
-  },
+  skills: [
+  'Proficiency in front-end web technologies: HTML, CSS, JavaScript',
+  'Experience with responsive web design and cross-browser compatibility',
+  'Basic understanding of version control systems, such as Git',
+  'Strong teamwork and communication skills through collaborative project work'
+  ],
+  experience: [
+  { job: 'Front End Developer / something.com', description: 'Highly skilled' },
+  { job: 'Mobile Developer / something.com', description: 'Highly skilled' },
+  { job: 'Web Developer / something.com', description: 'Highly skilled' }
+  ],
   awards: [
-    'Dean\'s List, USPF',
-    '3rd place in CCS Hackathon'
+  'Dean\'s List, USPF',
+  '3rd place in CCS Hackathon'
   ],
   personalInfo: {
-    name: 'SHAN JOSEF JAMIL N. MOLIVA',
-    address: 'cebu city, america.',
-    phoneNumber: '09132341',
-    email: 'Smoliva@uspf.edu.ph',
-    birthdate: 'December 13, 2000',
-    sex: 'Male'
-  },
-
-};
+  name: 'SHAN JOSEF JAMIL N. MOLIVA',
+  address: 'cebu city, america.',
+  phoneNumber: '09132341',
+  email: 'Smoliva@uspf.edu.ph',
+  birthdate: 'December 13, 2000',
+  sex: 'Male'
+  }
+  };
 
 // Enable CORS for all routes
 app.use(cors());
